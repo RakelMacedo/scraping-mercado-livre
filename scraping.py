e529eb80106eaed95725e333891090b39281bad9
Rakel <macedorakel35@gmail.com>
2022-09-21 13:48:56 -0300

    scraping

--- scraping.py
+++ scraping.py
@@ -1,38 +1,28 @@
-### Preencha os blocos faltantes de código abaixo:
-
-# Importando as bibliotecas 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Pegando a url 
-url_mercado_livre = "https://www.mercadolivre.com.br"
 
-# Realizando o requests
+url_mercado_livre = "https://www.mercadolivre.com.br"
 response = requests.get(url_mercado_livre)
 
-### Faça o PRINT do valor STATUS_CODE do response ### 
 status_code = response.status_code
 print(f'STATUS CODE {status_code}\n')
 
-# Transformando em HTML 
 mercado_livre_html = response.text
 soup = BeautifulSoup(mercado_livre_html, "html.parser")
 
-### Pegue os primeiros 5 produtos da categoria "Baseada na sua última visita" ###
+
 produtos = soup.find(class_="slick-track")
 produtos = produtos.find_all("div", class_="slick-slide slick-active")
 
-### Pegue as informações de: Preço, Número de Parcelas e construa o valor total Parcelado. ###
 
 nomes = []
-
 for produto in produtos:
   nome = produto.find(class_="ui-item__title").text
   nomes.append(nome)
 
 
-# Pegando preços, parcelas e total parcelado
 precos = []
 parcelas = []
 total_parcelados = []
@@ -56,7 +46,6 @@ for produto in produtos:
 
   total_parcelados.append(multiplicacao)
 
-### Faça um DataFrame com as informações coletadas. ###
 
 dicionario = {'PRODUTOS': nomes,'PRECOS': precos, 'PARCELAS': parcelas, 'TOTAL_PARCELADO': total_parcelados} 
 df = pd.DataFrame(dicionario) 
